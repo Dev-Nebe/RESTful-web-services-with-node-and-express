3f481a30659486722d52e8c97f0f1dafa4d8bb8e
Dev-Nebe <cp.nebe@gmail.com>
2019-11-17 22:12:07 +0100

    Setup tooling and implemented GET verb

---
+++ books.json.js
@@ -0,0 +1,23 @@
+db.books.insertMany([
+  {
+    title: 'War and Peace',
+    genre: 'Historical Fiction',
+    author: 'Lev Nikolayevich Tolstoy',
+    bookId: 656,
+    read: false
+  },
+  {
+    title: 'Les Misérables',
+    genre: 'Historical Fiction',
+    author: 'Victor Hugo',
+    bookId: 24280,
+    read: false,
+  },
+  {
+    title: 'The Wind in the Willows',
+    genre: 'Fantasy',
+    author: 'Kenneth Grahame',
+    bookId: 5659,
+    read: false
+  },
+]);
